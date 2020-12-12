4cd18d7a36e3229f8a0bfa41d5e6d5533afd0904
Shen Jun Wu <shenfive@gmail.com>
2020-12-12 13:37:58 +0800

    init

--- MyImageFromNet/ViewController.swift
+++ MyImageFromNet/ViewController.swift
@@ -8,10 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var theImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let imageUrlString = "https://i3.read01.com/SIG=nlj5f4/304a366b324159533874.jpg"
+        
+        if let url = URL(string: imageUrlString){
+            do {
+                let data = try Data(contentsOf: url)
+                theImageView.image = UIImage(data: data)
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+        
+        
     }
 
 
