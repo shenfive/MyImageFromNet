f327797ee44fbc0938667d8a787ff1f3b89e52ac
Shen Jun Wu <shenfive@gmail.com>
2020-12-12 14:20:40 +0800

    c

--- MyImageFromNet/ViewController.swift
+++ MyImageFromNet/ViewController.swift
@@ -10,8 +10,22 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var theImageView: UIImageView!
     
+    @IBOutlet weak var containerView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        containerView.clipsToBounds = false
+        containerView.backgroundColor = UIColor.clear
+        containerView.layer.shadowRadius = 20         //陰影
+        containerView.layer.shadowOpacity = 0.6;
+        containerView.layer.shadowColor = UIColor.gray.cgColor
+        containerView.layer.shadowOffset = CGSize(width: 10, height: 10)
+
+        
+        theImageView.clipsToBounds = true
+        theImageView.layer.cornerRadius = theImageView.frame.width / 2
+        theImageView.layer.borderColor = UIColor.blue.cgColor
+        theImageView.layer.borderWidth = 3
         
         let imageUrlString = "https://i3.read01.com/SIG=nlj5f4/304a366b324159533874.jpg"
         print("start:\(Date().timeIntervalSince1970)")
